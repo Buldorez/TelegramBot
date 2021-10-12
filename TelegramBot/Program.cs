242e3b0a674901ff0d4ec4487ea3b985282716a2
Андрей Галеев <galeev.kamensk33@mail.ru>
2021-10-12 23:48:40 +0500

    Добавьте файлы проекта.

---
+++ TelegramBot/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types.Enums;
+
+
+namespace TelegramBot
+{
+    class Program
+    {
+        static TelegramBotClient Bot;
+        static void Main(string[] args)
+        {
+            Bot = new TelegramBotClient("2051673379:AAGbbAbFBdNcCLokBPmQsC8FYZRAu4EfBpU");
+
+            Bot.OnMessage += BotOnMessageReceived;
+            Bot.OnCallbackQuery += BotOnCallbackQueryReceived;
+
+            var me = Bot.GetMeAsync().Result;
+
+            Console.WriteLine(me.FirstName);
+
+            Bot.StartReceiving();
+
+            Console.ReadLine();
+            Bot.StopReceiving();
+        }
+
+        private static void BotOnCallbackQueryReceived(object sender, Telegram.Bot.Args.CallbackQueryEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void BotOnMessageReceived(object sender, Telegram.Bot.Args.MessageEventArgs e)
+        {
+            var message = e.Message;
+
+            if ( message == null || message.Type != MessageType.Text)
+                return;
+            string name = $"{message.From.FirstName} {message.From.LastName}";
+
+            Console.WriteLine($"{name} отправил сообщение: '{message.Text}'");
+
+            switch (message.Text)
+            {
+                case "/start":
+
+                    break;
+                    case  "/keyboard":
+                        break;
+            }
+        }
+    }
+}
